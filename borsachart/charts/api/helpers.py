f45c5430a2bb74435dbc93abec778e9fefd38479
azaleas <azaleas@2die4.com>
2017-06-06 13:57:00 +0400

    added helper functions for ticker retrieval

---
+++ borsachart/charts/api/helpers.py
@@ -0,0 +1,34 @@
+# Helper functions for API
+
+import time
+from datetime import date, timedelta, datetime
+
+from django.utils import timezone
+
+from ..models import Ticker
+
+from .quandl import get_ticker
+
+def get_ticker_data(ticker):
+    """
+    Get the ticker data from DB if it's not older than a day.
+    Otherwise, get the data from Quandl
+    """
+    date_cached = timezone.now() - timedelta(days=1)
+    ticker_data = Ticker.objects.filter(
+                ticker=ticker, 
+                updated_date__gte=date_cached,
+            )
+
+    if ticker_data:
+        pass
+    else:
+        ticker_data = get_ticker_data_quandl(ticker)
+
+def get_ticker_data_quandl(ticker):
+    """
+    Get the ticker data from Quandl. Limit the data to one year
+    """
+    start_date = (timezone.now() - timedelta(days=365)).strftime("%Y%m%d")
+    end_date = timezone.now().strftime("%Y%m%d")
+    ticker_quandl_data = get_ticker(ticker, start_date, end_date)
--- borsachart/charts/api/views.py
+++ borsachart/charts/api/views.py
@@ -5,6 +5,8 @@ from rest_framework.decorators import detail_route, list_route
 from ..models import Ticker
 from .serializers import TickerSerializer
 
+from .helpers import get_ticker_data
+
 class ChartsViewSet(viewsets.ViewSet):
     """
     Viewset to deal with search and view method
@@ -23,6 +25,7 @@ class ChartsViewSet(viewsets.ViewSet):
             serializer = TickerSerializer(data=request.data)
             if serializer.is_valid():
                 ticker = request.data['ticker']
+                json_data = get_ticker_data(ticker)
                 return Response(ticker, status.HTTP_200_OK)
             else:
                 return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
