eccfae0f7283b55dcbff522b256d299275335a94
azaleas <azaleas@2die4.com>
2017-06-11 20:42:57 +0400

    added ticker inputs

--- borsachart/frontend/src/components/InputComponent.js
+++ borsachart/frontend/src/components/InputComponent.js
@@ -27,7 +27,7 @@ const InputComponent = (props) => {
                     <input 
                         type="button"
                         value="Submit"
-                        className="block-form-submit"
+                        className="block-form-submit btn"
                         onClick={props.onInputSubmit}/>
                 </div>
                 <p className={"notfound " + (props.notFound ? "show" : "")} >
@@ -35,7 +35,44 @@ const InputComponent = (props) => {
                 </p>
             </div>
             <div className="inputs-block column">
-                hello
+                <div className="tickers">
+                    {
+                        props.data.map((el, index) => {
+                            let lastColumn = el.data.datatable.data.length - 1;
+                            console.log(lastColumn);
+                            return(
+                                <div 
+                                    key={index}
+                                    className="ticker"
+                                    >
+                                    <div className="ticker-text">
+                                        <h4 className="ticker-text--title">{el.ticker}</h4>
+                                        <hr/>
+                                        <h5 className="ticker-text--latestdata">Latest data:</h5>
+                                        <div className="ticker-text--data">
+                                            <p className="ticker-text--info">
+                                                Open: {el.data.datatable.data[lastColumn][2]}
+                                            </p>
+                                            <p className="ticker-text--info">
+                                                High: {el.data.datatable.data[lastColumn][3]}
+                                            </p>
+                                            <p className="ticker-text--info">
+                                                Low: {el.data.datatable.data[lastColumn][4]}
+                                            </p>
+                                            <p className="ticker-text--info">
+                                                Close: {el.data.datatable.data[lastColumn][5]}
+                                            </p>
+                                            <p className="ticker-text--info">
+                                                Volume: {el.data.datatable.data[lastColumn][6]}
+                                            </p>
+                                        </div>
+                                        <p className="ticker--remove btn">Remove</p>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
             </div>
         </div> 
     );
