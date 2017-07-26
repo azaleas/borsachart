aeb2a8f889aa9bfc40a0174e352be10442a2d056
azaleas <azaleas@2die4.com>
2017-07-26 17:31:03 +0400

    added some comments

--- borsachart/frontend/src/utils/variables.js
+++ borsachart/frontend/src/utils/variables.js
@@ -1,9 +1,12 @@
-let URL = window.location.hostname + '/charts/';
+/*
+URL for production server
+*/
+// let URL = window.location.hostname + '/charts/';
 
 /*
 URL for local server
-let URL = window.location.hostname + ':8000/charts/';
 */
+let URL = window.location.hostname + ':8000/charts/';
 let variables = {
     URL,
 }
